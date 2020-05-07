f9f32ade55a7e29c620b3a6921e02c41a4fdf2be
HunterSandlin <62820211+HunterSandlin@users.noreply.github.com>
2020-05-06 21:54:22 -0400

    Added files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function allowDrop(event) {
+    event.preventDefault();
+}
+  
+let drag = (event) => {
+    event.target.id = "inMotion";
+    event.dataTransfer.setData("text", "inMotion");
+    drag.lastEvent = event.currentTarget.parentNode;
+    drag.lastPicture = event.target;
+}
+
+
+/* issue in this function */
+function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text");
+    //if it is coming from the bank
+    if (drag.lastEvent.className === "div_pickup") {
+        //clone node to make a copy BUT now it has the same id
+        let clone = document.getElementById(data).cloneNode(false);
+        clone.id = '';
+        event.target.appendChild(clone);
+    } else if (drag.lastEvent.className === "div_dropbox") {
+        event.target.appendChild(document.getElementById(data));
+    }
+    drag.lastPicture.id = '';
+}
